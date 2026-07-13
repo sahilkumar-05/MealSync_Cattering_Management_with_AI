8fc19e43caae1602cf4c795b14344d8ce1923c46
Sahil Kumar <129257408+sahilkumar-05@users.noreply.github.com>
2026-07-13 18:16:10 +0500

    Update CORS origin for WebSocket gateway

--- backend/src/notifications/notifications.gateway.ts
+++ backend/src/notifications/notifications.gateway.ts
@@ -13,7 +13,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 @WebSocketGateway({
   cors: {
-    origin: ['http://localhost:5173', 'https://meal-sync-cattering-management-with-theta.vercel.app/login'],
+    origin: ['http://localhost:5173', 'https://meal-sync-cattering-management-with-theta.vercel.app'],
   },
 })
 export class NotificationsGateway implements OnGatewayConnection, OnGatewayDisconnect {
